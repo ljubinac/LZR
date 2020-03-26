cd98c531f315d1cd06f6e2d31a7090b60902e5d1
ljubinac <57670199+ljubinac@users.noreply.github.com>
2020-03-26 18:07:09 +0100

    creating main activity

---
+++ app/src/main/java/com/hfad/lzr/CreatingMatch.java
@@ -0,0 +1,21 @@
+package com.hfad.lzr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreatingMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creating_match);
+    }
+
+    public void addTeam(View view){
+        Intent intent = new Intent(this, CreateTeam.class);
+        startActivity(intent);
+    }
+}
