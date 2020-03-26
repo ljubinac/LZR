5bd2aa9d3b817a21f11b50260d369c3b59a78dd7
ljubinac <57670199+ljubinac@users.noreply.github.com>
2020-03-26 22:52:13 +0100

    add players update

--- app/src/main/java/com/hfad/lzr/CreateTeamActivity.java
+++ app/src/main/java/com/hfad/lzr/CreateTeamActivity.java
@@ -12,14 +12,25 @@ import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.hfad.lzr.model.Player;
 import com.hfad.lzr.model.Team;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateTeamActivity extends AppCompatActivity {
 
     Button saveTeam;
     EditText teamName;
-    DatabaseReference databaseReference;
+    DatabaseReference databaseReferencePlayers;
+    DatabaseReference databaseReferenceTeams;
     Spinner leagueName;
+
+    EditText playerNumber;
+    EditText playerName;
+    Button addPlayer;
+
+    List<Player> players = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +40,20 @@ public class CreateTeamActivity extends AppCompatActivity {
         saveTeam = findViewById(R.id.save_team);
         teamName = findViewById(R.id.team_name);
         leagueName = findViewById(R.id.choose_league);
-        databaseReference = FirebaseDatabase.getInstance().getReference("teams");
+        databaseReferenceTeams = FirebaseDatabase.getInstance().getReference("teams");
+
+        addPlayer = findViewById(R.id.player_add_btn);
+        playerNumber = findViewById(R.id.player_number_edt);
+        playerName = findViewById(R.id.player_name_edt);
+        databaseReferencePlayers = FirebaseDatabase.getInstance().getReference("players");
+
+        addPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                addPlayer();
+            }
+        });
         saveTeam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,13 +68,38 @@ public class CreateTeamActivity extends AppCompatActivity {
     private void addTeam() {
         String name = teamName.getText().toString();
         String league = String.valueOf(leagueName.getSelectedItem());
-        if (!TextUtils.isEmpty(name)) {
-            String id = databaseReference.push().getKey();
+        if (!TextUtils.isEmpty(name) && !TextUtils.isEmpty(league)) {
+            String id = databaseReferenceTeams.push().getKey();
             Team team = new Team(id, name, league);
-            databaseReference.child(id).setValue(team);
+            databaseReferenceTeams.child(id).setValue(team);
+
+            for (Player p : players){
+                String idPlayer = databaseReferencePlayers.push().getKey();
+                p.setId(idPlayer);
+                p.setTeamId(id);
+
+                databaseReferencePlayers.child(idPlayer).setValue(p);
+            }
             Toast.makeText(this, "Team added!", Toast.LENGTH_LONG).show();
         } else {
             Toast.makeText(this, "Team not added!", Toast.LENGTH_LONG).show();
         }
     }
+
+    private void addPlayer(){
+        String number = playerNumber.getText().toString();
+        String name = playerName.getText().toString();
+        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(number)){
+
+            Player player = new Player(name, number);
+
+            players.add(player);
+
+            // databaseReference.child(id).setValue(player);
+            Toast.makeText(this, "Player added!", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Player not added!", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
---
+++ app/src/main/java/com/hfad/lzr/model/Player.java
@@ -0,0 +1,56 @@
+package com.hfad.lzr.model;
+
+public class Player {
+
+    private String id;
+    private String nameAndLastname;
+    private String number;
+
+    public String getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(String teamId) {
+        this.teamId = teamId;
+    }
+
+    private String teamId;
+
+    public Player() {
+    }
+
+    public Player(String id, String nameAndLastname, String number) {
+        this.id = id;
+        this.nameAndLastname = nameAndLastname;
+        this.number = number;
+    }
+
+    public Player(String name, String number) {
+        this.nameAndLastname = name;
+        this.number = number;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNameAndLastname() {
+        return nameAndLastname;
+    }
+
+    public void setNameAndLastname(String nameAndLastname) {
+        this.nameAndLastname = nameAndLastname;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
