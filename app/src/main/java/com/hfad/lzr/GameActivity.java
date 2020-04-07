513c6833d6bae461c0e1cbfe15c4d13cd2b0d622
ljubinac <57670199+ljubinac@users.noreply.github.com>
2020-04-07 23:27:42 +0200

    GameActivity almost finished 2

--- app/src/main/java/com/hfad/lzr/GameActivity.java
+++ app/src/main/java/com/hfad/lzr/GameActivity.java
@@ -48,6 +48,26 @@ public class GameActivity extends AppCompatActivity {
     int resA;
     int resB;
     boolean currentTeam;
+    LinearLayout ll3pm;
+    LinearLayout ll3pa;
+    TextView tv3pm;
+    TextView tv3pa;
+    LinearLayout ll1pm;
+    LinearLayout ll1pa;
+    TextView tv1pm;
+    TextView tv1pa;
+    LinearLayout llOffReb;
+    LinearLayout llDefReb;
+    TextView tvOffReb;
+    TextView tvDefReb;
+    LinearLayout llAsist;
+    LinearLayout llTurnov;
+    TextView tvAsist;
+    TextView tvTurnov;
+    LinearLayout llBlock;
+    LinearLayout llFoul;
+    TextView tvBlock;
+    TextView tvFoul;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,6 +94,26 @@ public class GameActivity extends AppCompatActivity {
         tv2pa = findViewById(R.id.tv2pa);
         resAtv = findViewById(R.id.scoreTeamA);
         resBtv = findViewById(R.id.scoreTeamB);
+        ll3pm = findViewById(R.id.ll_3PM);
+        ll3pa = findViewById(R.id.ll_3PA);
+        tv3pm = findViewById(R.id.tv3pm);
+        tv3pa = findViewById(R.id.tv3pa);
+        ll1pm = findViewById(R.id.ll_1PM);
+        ll1pa = findViewById(R.id.ll_1PA);
+        tv1pm = findViewById(R.id.tv1pm);
+        tv1pa = findViewById(R.id.tv1pa);
+        llOffReb = findViewById(R.id.ll_offReb);
+        llDefReb = findViewById(R.id.ll_defReb);
+        tvOffReb = findViewById(R.id.offRebTV);
+        tvDefReb = findViewById(R.id.defRebTV);
+        llAsist = findViewById(R.id.ll_assist);
+        llTurnov = findViewById(R.id.ll_turnovers);
+        tvAsist = findViewById(R.id.assistTV);
+        tvTurnov = findViewById(R.id.turnoversTV);
+        llBlock = findViewById(R.id.ll_block);
+        llFoul = findViewById(R.id.ll_foul);
+        tvBlock = findViewById(R.id.blockTV);
+        tvFoul = findViewById(R.id.foulTV);
 
         resA = 0;
         resB = 0;
@@ -105,6 +145,91 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
+        ll3pm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setPm3(current.getPm3() + 1);
+                current.setPa3(current.getPa3() + 1);
+                tv3pm.setText(String.valueOf(current.getPm3()));
+                tv3pa.setText(String.valueOf(current.getPa3()));
+                setRes(3);
+            }
+        });
+
+        ll3pa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setPa3(current.getPa3() + 1);
+                tv3pa.setText(String.valueOf(current.getPa3()));
+            }
+        });
+
+        ll1pm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setPm1(current.getPm1() + 1);
+                current.setPa1(current.getPa1() + 1);
+                tv1pm.setText(String.valueOf(current.getPm1()));
+                tv1pa.setText(String.valueOf(current.getPa1()));
+                setRes(1);
+            }
+        });
+
+        ll1pa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setPa1(current.getPa1() + 1);
+                tv1pa.setText(String.valueOf(current.getPa1()));
+            }
+        });
+
+        llOffReb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setOffReb(current.getOffReb() + 1);
+                tvOffReb.setText(String.valueOf(current.getOffReb()));
+            }
+        });
+
+        llDefReb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setDefReb(current.getDefReb() + 1);
+                tvDefReb.setText(String.valueOf(current.getDefReb()));
+            }
+        });
+
+        llAsist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setAsist(current.getAsist() + 1);
+                tvAsist.setText(String.valueOf(current.getAsist()));
+            }
+        });
+
+        llTurnov.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setTurnover(current.getTurnover() + 1);
+                tvTurnov.setText(String.valueOf(current.getTurnover()));
+            }
+        });
+
+        llBlock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setBlock(current.getBlock() + 1);
+                tvBlock.setText(String.valueOf(current.getBlock()));
+            }
+        });
+
+        llFoul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                current.setFoul(current.getFoul() + 1);
+                tvFoul.setText(String.valueOf(current.getFoul()));
+            }
+        });
 
 
     }
@@ -122,6 +247,21 @@ public class GameActivity extends AppCompatActivity {
     public void setValues(){
         tv2pm.setText(String.valueOf(current.getPm2()));
         tv2pa.setText(String.valueOf(current.getPa2()));
+
+        tv3pm.setText(String.valueOf(current.getPm3()));
+        tv3pa.setText(String.valueOf(current.getPa3()));
+
+        tv1pm.setText(String.valueOf(current.getPm1()));
+        tv1pa.setText(String.valueOf(current.getPa1()));
+
+        tvOffReb.setText(String.valueOf(current.getOffReb()));
+        tvDefReb.setText(String.valueOf(current.getDefReb()));
+
+        tvAsist.setText(String.valueOf(current.getAsist()));
+        tvTurnov.setText(String.valueOf(current.getTurnover()));
+
+        tvBlock.setText(String.valueOf(current.getBlock()));
+        tvFoul.setText(String.valueOf(current.getFoul()));
     }
 
 
